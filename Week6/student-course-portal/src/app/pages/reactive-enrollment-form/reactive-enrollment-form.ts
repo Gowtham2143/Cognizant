5f48254fc9052d1c25786dfe1e372fa3509c0f13
Gopisetti Gowtham <gopisettigowtham@Gopisettis-MacBook-Air.local>
2026-07-29 21:57:11 +0530

    Completed Angular Hands-On 5 Part 1

---
+++ Week6/student-course-portal/src/app/pages/reactive-enrollment-form/reactive-enrollment-form.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-reactive-enrollment-form',
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './reactive-enrollment-form.html',
+  styleUrl: './reactive-enrollment-form.css',
+})
+export class ReactiveEnrollmentForm implements OnInit {
+  enrollForm!: FormGroup;
+  submitted = false;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.enrollForm = this.fb.group({
+      studentName: ['', [Validators.required, Validators.minLength(3)]],
+      studentEmail: ['', [Validators.required, Validators.email]],
+      courseId: [null, Validators.required],
+      preferredSemester: ['Odd', Validators.required],
+      agreeToTerms: [false, Validators.requiredTrue],
+    });
+  }
+
+  onSubmit(): void {
+    console.log(this.enrollForm.value);
+    console.log(this.enrollForm.valid);
+
+    if (this.enrollForm.valid) {
+      this.submitted = true;
+    }
+  }
+
+  get f() {
+    return this.enrollForm.controls;
+  }
+}
